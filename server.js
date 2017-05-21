d3e9cafa12806a00d61a8cd996dd052183b695aa
Brendan Cannell <brendan@cannells.com>
2017-05-21 12:31:28 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const url  = require('url');
+
+const server = http.createServer(function (req, res) {
+    const input = decodeURIComponent(url.parse(req.url).pathname).slice(1);
+    const inputIsUnixTime = input.match(/^\d*$/);
+
+    var inputDate;
+    if (inputIsUnixTime) {
+	const parsed = Number.parseInt(input);
+
+	if (!Number.isNaN(parsed)) {
+	    inputDate = new Date(parsed * 1000);
+	}
+    } else {
+	inputDate = new Date(input);
+    }
+
+    const output = (inputDate && (inputDate.toDateString() != "Invalid Date")) ? {
+	unix:    Math.floor(inputDate.getTime() / 1000),
+	natural: inputIsUnixTime ? inputDate.toDateString() : input
+    } : {
+	unix:    null,
+	natural: null
+    };
+    
+    res.end(JSON.stringify(output));
+});
+
+server.listen(24601);
